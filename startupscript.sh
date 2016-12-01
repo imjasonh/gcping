b54b6a6e95a7b47a44f75bfcfa9dd0fe3ce41c0a
Jason Hall <jasonhall@google.com>
2016-12-01 11:37:54 -0800

    Fix startup, stop after 30s

--- startupscript.sh
+++ startupscript.sh
@@ -7,16 +7,15 @@ sudo apt-get install python2.7
 # Start simple HTTP server with CORS header set.
 echo "pong" > ping
 cat << EOF > cors-http-server.py
-#! /usr/bin/env python
+#!/usr/bin/env python
+import SimpleHTTPServer
 
-from SimpleHTTPServer import SimpleHTTPRequestHandler, test
-
-class CORSHTTPRequestHandler(SimpleHTTPRequestHandler):
+class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def end_headers(self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        super(CORSHTTPRequestHandler, self).end_headers(self)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    test(HandlerClass=CORSHTTPRequestHandler)
+    SimpleHTTPServer.test(HandlerClass=MyHTTPRequestHandler)
 EOF
-sudo python cors-http-server.py
+sudo python cors-http-server.py 80

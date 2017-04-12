278e128fccc138007d76d5032d244c8db32fc31c
Jason Hall <jasonhall@google.com>
2017-04-12 16:45:53 -0400

    It works, now with a custom network

--- create_addrs.sh
+++ create_addrs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export CLOUDSDK_CORE_PROJECT=gcping-1369
-REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1 asia-southeast-1"
+REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1 asia-southeast1"
 
 # TODO: Delete addresses?
 
--- deploy_vms.sh
+++ deploy_vms.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
+set -x
+
 export CLOUDSDK_CORE_PROJECT=gcping-1369
-REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1 asia-southeast-1"
+REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1 asia-southeast1"
 
 # Delete network and recreate it with subnets for each region.
+FWR_NAME=network-allow-http
 NETWORK_NAME=network
 SUBNET_NAME=subnet
-gcloud compute networks delete $NETWORK_NAME
+gcloud -q compute firewall-rules delete $FWR_NAME || true
+gcloud -q compute networks delete $NETWORK_NAME || true
 gcloud compute networks create $NETWORK_NAME \
   --mode=custom \
   --description="Non-default network"
+gcloud compute firewall-rules create network-allow-http \
+  --network=network \
+  --allow=tcp:80 \
+  --target-tags=http-server
 
 part=100
 for r in $REGIONS; do
-  gcloud compute networks subnet create $SUBNET_NAME \
-    --network=$NETWORK \
-    --range="10.$part.0.0/0" \
+  gcloud compute networks subnets create $SUBNET_NAME \
+    --region=$r \
+    --network=$NETWORK_NAME \
+    --range="10.$part.0.0/20"
   part=$((part+2))
 done
 

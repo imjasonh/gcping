a1b54dba8dfccb2a1e9f1e4bf9c8f2dfb306a34c
Jason Hall <jasonhall@google.com>
2017-04-12 15:49:46 -0400

    Add asia-southeast1 to create_addrs and deploy_vms

--- create_addrs.sh
+++ create_addrs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export CLOUDSDK_CORE_PROJECT=gcping-1369
-REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1"
+REGIONS="us-central1 us-east1 us-west1 europe-west1 asia-east1 asia-northeast1 asia-southeast-1"
+
+# TODO: Delete addresses?
 
 # Create static addresses for each region.
 for r in $REGIONS; do

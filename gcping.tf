64085285c9772a5b9a711130d5a86eb76042ab0a
Jason Hall <jasonhall@google.com>
2020-11-22 21:05:52 -0500

    Add LBs per region for domain mapping (e.g., asia-north1.gcping.com)

--- gcping.tf
+++ gcping.tf
@@ -35,7 +35,7 @@ variable "project" {
 
 variable "domain" {
   type    = string
-  default = "global.gcping.com."
+  default = "gcping.com"
 }
 
 // TODO: generate this
@@ -76,8 +76,8 @@ resource "google_project_service" "run" {
   service = "run.googleapis.com"
 }
 
-// Deploy ${image} to each region.
-resource "google_cloud_run_service" "default" {
+// Deploy image to each region.
+resource "google_cloud_run_service" "regions" {
   for_each = toset(var.regions)
   name     = each.key
   location = each.key
@@ -119,98 +119,128 @@ resource "google_cloud_run_service" "default" {
 resource "google_cloud_run_service_iam_member" "allUsers" {
   for_each = toset(var.regions)
 
-  service  = google_cloud_run_service.default[each.key].name
+  service  = google_cloud_run_service.regions[each.key].name
   location = each.key
   role     = "roles/run.invoker"
   member   = "allUsers"
 
-  depends_on = [google_cloud_run_service.default]
-}
-
-// Print regional Cloud Run service URLs.
-output "urls" {
-  value = {
-    for svc in google_cloud_run_service.default :
-    svc.name => svc.status[0].url
-  }
-}
-
-////// Load Balancer config
-
-// Print the global LB's IP address, to plug into DNS.
-output "global-ip" {
-  value = google_compute_global_forwarding_rule.default.ip_address
-}
-
-resource "google_compute_global_forwarding_rule" "default" {
-  name       = "global-rule"
-  target     = google_compute_target_https_proxy.default.id
-  port_range = "443"
-}
-
-resource "google_compute_managed_ssl_certificate" "default" {
-  provider = google-beta
-
-  name = "ssl-cert"
-
-  managed {
-    domains = [var.domain]
-  }
-}
-
-resource "google_dns_managed_zone" "zone" {
-  provider = google-beta
-
-  name     = "dnszone"
-  dns_name = var.domain
-}
-
-resource "google_dns_record_set" "set" {
-  provider = google-beta
-
-  name         = var.domain
-  type         = "A"
-  ttl          = 3600
-  managed_zone = google_dns_managed_zone.zone.name
-  rrdatas      = [google_compute_global_forwarding_rule.default.ip_address]
-}
-
-resource "google_compute_target_https_proxy" "default" {
-  provider = google-beta
-
-  name             = "https-proxy"
-  url_map          = google_compute_url_map.default.id
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
-}
-
-resource "google_compute_url_map" "default" {
-  provider = google-beta
-
-  name            = "url-map"
-  description     = "a description"
-  default_service = google_compute_backend_service.default.id
+  depends_on = [google_cloud_run_service.regions]
 }
 
 // Create a regional network endpoint group (NEG) for each regional Cloud Run service.
-resource "google_compute_region_network_endpoint_group" "default" {
+resource "google_compute_region_network_endpoint_group" "regions" {
   for_each = toset(var.regions)
 
-  name                  = "default"
+  name                  = each.key
   network_endpoint_type = "SERVERLESS"
   region                = each.key
   cloud_run {
-    service = google_cloud_run_service.default[each.key].name
+    service = google_cloud_run_service.regions[each.key].name
   }
 }
 
+////// Regional Domain + Load Balancer config
+
+resource "google_compute_global_forwarding_rule" "regions" {
+  for_each = toset(var.regions)
+
+  name       = each.key
+  target     = google_compute_target_https_proxy.regions[each.key].id
+  port_range = "443"
+}
+
+// Print regional LB IP addresses.
+output "regions" {
+  value = {
+    for fwd in google_compute_global_forwarding_rule.regions :
+    fwd.name => fwd.ip_address
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "regions" {
+  for_each = toset(var.regions)
+  provider = google-beta
+
+  name = each.key
+  managed {
+    domains = ["${each.key}.${var.domain}."]
+  }
+}
+
+resource "google_compute_target_https_proxy" "regions" {
+  for_each = toset(var.regions)
+  provider = google-beta
+
+  name             = each.key
+  url_map          = google_compute_url_map.regions[each.key].id
+  ssl_certificates = [google_compute_managed_ssl_certificate.regions[each.key].id]
+}
+
+resource "google_compute_url_map" "regions" {
+  for_each = toset(var.regions)
+  provider = google-beta
+
+  name            = each.key
+  description     = "a description"
+  default_service = google_compute_backend_service.regions[each.key].id
+}
+
+resource "google_compute_backend_service" "regions" {
+  for_each = toset(var.regions)
+
+  name   = each.key
+  enable_cdn = true
+  backend {
+    group = google_compute_region_network_endpoint_group.regions[each.key].id
+  }
+}
+
+////// Global Domain + Load Balancer config
+
+resource "google_compute_global_forwarding_rule" "global" {
+  name       = "global"
+  target     = google_compute_target_https_proxy.global.id
+  port_range = "443"
+}
+
+// Print global LB IP address.
+output "global" {
+  value = google_compute_global_forwarding_rule.global.ip_address
+}
+
+resource "google_compute_managed_ssl_certificate" "global" {
+  provider = google-beta
+
+  name = "global"
+  managed {
+    domains = ["global.${var.domain}."]
+  }
+}
+
+resource "google_compute_target_https_proxy" "global" {
+  provider = google-beta
+
+  name             = "global"
+  url_map          = google_compute_url_map.global.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.global.id]
+}
+
+resource "google_compute_url_map" "global" {
+  provider = google-beta
+
+  name            = "global"
+  description     = "a description"
+  default_service = google_compute_backend_service.global.id
+}
+
 // Create a global backend service with a backend for each regional NEG.
-resource "google_compute_backend_service" "default" {
-  name       = "cloudrun-backend-service" // TODO: rename
+resource "google_compute_backend_service" "global" {
+  name       = "global"
   enable_cdn = true
 
   // Add a backend for each regional NEG.
   dynamic "backend" {
-    for_each = google_compute_region_network_endpoint_group.default
+    for_each = google_compute_region_network_endpoint_group.regions
     content {
       group = backend.value["id"]
     }

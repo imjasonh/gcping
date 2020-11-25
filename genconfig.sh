ee0f6b510b74f69d3f49b07b85c2a00294866468
Jason Hall <jasonhall@google.com>
2020-11-25 14:48:23 -0500

    fix global in genconfig.sh

--- genconfig.sh
+++ genconfig.sh
@@ -4,10 +4,8 @@ out=${1:-cmd/ping/kodata/config.js}
 
 echo "writing to $out"
 
-global=$(terraform output global)
-
 echo "let _URLS = {" > $out
-/bin/bash: :wqa: command not found
+echo '  "global": "https://global.gcping.com",' >> $out
 terraform output services | tail -n+2 | sed -e 's/ =/:/g' | sed -e 's/"$/",/g' | sed -e 's/}/};/g' >> $out
 
 cat $out
